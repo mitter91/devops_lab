1669fc5d55484b5bee83478cebdb9898664b9566
mitter91 <55717151+mitter91@users.noreply.github.com>
2020-03-03 22:29:17 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,8 +1,8 @@
 # !/usr/bin/python3
 
 N = input()
-# if N == N[::-1]:
-#    print("yes")
-# else:
-#    print("no")
-print(N == N[::-1])
+if N == N[::-1]:
+   print("yes")
+else:
+   print("no")
+# print(N == N[::-1])
